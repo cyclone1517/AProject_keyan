513263660768d29d18ed234f1033555b168407a9
m15116200773_1@163.com <m15116200773_1@163.com>
2017-12-04 22:30:28 +0800

    add WorkTpe

--- src/Bean/WorkTpe.java
+++ src/Bean/WorkTpe.java
@@ -1,5 +1,19 @@
 package Bean;
 
 public class WorkTpe {
-
+	  String Type_ID;              
+	  String Type_name;
+	public String getType_ID() {
+		return Type_ID;
+	}
+	public void setType_ID(String type_ID) {
+		Type_ID = type_ID;
+	}
+	public String getType_name() {
+		return Type_name;
+	}
+	public void setType_name(String type_name) {
+		Type_name = type_name;
+	}     
+	  
 }
