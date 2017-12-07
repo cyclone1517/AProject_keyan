af25b7916eb2ff769001254c216ec6d387ea9095
m15116200773_1@163.com <m15116200773_1@163.com>
2017-12-07 12:44:59 +0800

    xml的操作

--- src/columnXML/CreatXML.java
+++ src/columnXML/CreatXML.java
@@ -4,11 +4,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.Node;
 import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 
@@ -35,12 +41,129 @@ public class CreatXML {
 			e.printStackTrace();
 		}
 	}
-	public void addlabel(String id){
-		
+	public void addTeaElement(String teaid){
+		SAXReader reader = new SAXReader();
+		try {
+		Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		//Node node = document.selectSingleNode("//title");
+		Element root = document.getRootElement();//根节点
+		Element teacher=root.addElement("teacher");
+		teacher.addAttribute("tid", teaid);
+		//指定文件输出的位置
+        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+        // 指定文本的写出的格式：
+        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+        format.setEncoding("UTF-8");
+        //1.创建写出对象
+        XMLWriter writer=new XMLWriter(out,format);
+        //2.写出Document对象
+		writer.write(document);
+        //3.关闭流
+        writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+        
+	}
+	
+	public void addWorElement(String teaid,String worid,String state){
+		SAXReader reader = new SAXReader();
+		try {
+		 Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]");
+		//Iterator iter = (Iterator) list.iterator();
+		 Element element=(Element)node;
+		 Element work=element.addElement("work");
+		 work.addAttribute("wid", worid);
+		 work.addAttribute("state", state);
+		//指定文件输出的位置
+	        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+	        // 指定文本的写出的格式：
+	        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+	        format.setEncoding("UTF-8");
+	        //1.创建写出对象
+	        XMLWriter writer=new XMLWriter(out,format);
+	        //2.写出Document对象
+			writer.write(document);			
+	        //3.关闭流
+	        writer.close();
+	        } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	public void addColElement(String teaid,String worid,String state,String colid){
+		SAXReader reader = new SAXReader();
+		try {
+		 Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]/work[@wid="+worid+" and @state="+state+"]");
+		//Iterator iter = (Iterator) list.iterator();
+		 Element element=(Element)node;
+		 Element column=element.addElement("column");
+		 column.addAttribute("cid", colid);
+		//指定文件输出的位置
+	        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+	        // 指定文本的写出的格式：
+	        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+	        format.setEncoding("UTF-8");
+	        //1.创建写出对象
+	        XMLWriter writer=new XMLWriter(out,format);
+	        //2.写出Document对象
+			writer.write(document);			
+	        //3.关闭流
+	        writer.close();
+	        } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	public void addColContent(String teaid,String worid,String state,String colid,String content){
+		SAXReader reader = new SAXReader();  
+		try {
+		 Document document = reader.read(new File("WorkColumn.xml"));
+		 Node node = document.selectSingleNode("//teacher[@tid="+teaid+"]/work[@wid="+worid+" and @state="+state+"]/column[@cid="+colid+"]"); 
+		 node.setText(content);
+		//指定文件输出的位置
+	        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+	        // 指定文本的写出的格式：
+	        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+	        format.setEncoding("UTF-8");
+	        //1.创建写出对象
+	        XMLWriter writer=new XMLWriter(out,format);
+	        //2.写出Document对象
+			writer.write(document);	
+	        //3.关闭流
+	        writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new CreatXML().createXML();
+		//new CreatXML().createXML();
+		//new CreatXML().addTeaElement("201526010308");
+		//new CreatXML().addWorElement("201526010308","1","1");
+		//new CreatXML().addColElement("201526010308","1","1","1");
+		String content="科研成果展示系统成果一Nullam hendrerit justo non leo aliquet imperdiet. Etiam in sagittis lectus. Suspendisse ultrices placerat accumsan. Mauris quis dapibus orci. In dapibus velit blandit pharetra tincidunt. Quisque non sapien nec lacus condimentum facilisis ut iaculis enim. Sed viverra interdum bibendum. Donec ac sollicitudin dolor. Sed fringilla vitae lacus at rutrum. Phasellus congue vestibulum ligula sed consequat.";
+		new CreatXML().addColContent("201526010308","1","1","1",content);
 	}
 
 }
---
+++ src/columnXML/delXML.java
@@ -0,0 +1,128 @@
+package columnXML;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+public class delXML {
+	public void delTeaElement(String teaid){
+		SAXReader reader = new SAXReader();
+		try {
+		Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]");
+		node.getParent().remove(node);
+		//指定文件输出的位置
+        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+        // 指定文本的写出的格式：
+        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+        format.setEncoding("UTF-8");
+        //1.创建写出对象
+        XMLWriter writer=new XMLWriter(out,format);
+        //2.写出Document对象
+		writer.write(document);			
+        //3.关闭流
+		writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void delWorElement(String teaid,String worid,String state){
+		SAXReader reader = new SAXReader();
+		try {
+		Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]/work[@wid="+worid+" and @state="+state+"]");
+		node.getParent().remove(node);
+		//指定文件输出的位置
+        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+        // 指定文本的写出的格式：
+        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+        format.setEncoding("UTF-8");
+        //1.创建写出对象
+        XMLWriter writer=new XMLWriter(out,format);
+        //2.写出Document对象
+		writer.write(document);			
+        //3.关闭流
+		writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void delColElement(String teaid,String worid,String state,String colid){
+		SAXReader reader = new SAXReader();
+		try {
+		Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]/work[@wid="+worid+" and @state="+state+"]/column[@cid="+colid+"]");
+		node.getParent().remove(node);
+		//指定文件输出的位置
+        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+        // 指定文本的写出的格式：
+        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+        format.setEncoding("UTF-8");
+        //1.创建写出对象
+        XMLWriter writer=new XMLWriter(out,format);
+        //2.写出Document对象
+		writer.write(document);			
+        //3.关闭流
+		writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void delColContent(String teaid,String worid,String state,String colid){
+		SAXReader reader = new SAXReader();
+		try {
+		Document document;
+		document = reader.read(new File("WorkColumn.xml"));
+		Node node= document.selectSingleNode("//teacher[@tid="+teaid+"]/work[@wid="+worid+" and @state="+state+"]/column[@cid="+colid+"]");
+		node.setText("");
+		//指定文件输出的位置
+        FileOutputStream out =new FileOutputStream("WorkColumn.xml");
+        // 指定文本的写出的格式：
+        OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+        format.setEncoding("UTF-8");
+        //1.创建写出对象
+        XMLWriter writer=new XMLWriter(out,format);
+        //2.写出Document对象
+		writer.write(document);			
+        //3.关闭流
+		writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new delXML().delTeaElement("201526010307");
+		new delXML().delColContent("201526010308","1","1","1");
+	}
+
+}
